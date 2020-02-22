3d34056d4a2f1b793e5aad58e54f7b4ec09eb884
saahilh <saahil_hamayun@yahoo.com>
2020-02-22 12:59:31 -0500

    Added html loader to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,14 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-        }
-      }
+        },
+      },
+      {
+        test: /\.html/,
+        use: {
+          loader: 'html-loader',
+        },
+      },
     ],
   },
 }
